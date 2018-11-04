d0341067472579b4055e384ae75114f5ae1aae15
aaron <aahuang@berkeley.edu>
2018-11-03 20:57:09 -0700

    asd

--- browser/server.js
+++ browser/server.js
@@ -6,7 +6,9 @@ app.use(express.static('./'))
 app.get('/', function(req, res){
 	res.sendfile('index.html')
 })
-
+app.get('/svg-test', function(req, res){
+	res.sendfile('another.svg')
+})
 app.listen(3000, function(){
 	console.log('running')
 })
